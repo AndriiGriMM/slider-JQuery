3d1bf4f1e862c47f198f0c6428444a3ff7851157
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-05 22:44:32 +0300

    homework  05.06.22-2

--- main.js
+++ main.js
@@ -2,41 +2,32 @@ let form = document.querySelector('.form')
 let submit = document.querySelector('.js--submit')
 let from = form.querySelector('.js--number')
 let fields = form.querySelectorAll('.input')
-let checkInput = function (){
-    const value = this.value;
-    if (value.length > 16) {
-        this.value = value.slice(0, 16);
-    }
-}
-let genErrors = function(text){
-    let error = document.createElement('div')
-        error.className="error"
-        from.style.border = "1px solid red"
-        error.innerHTML= text
-        return error
-}
- let removeErr= function(){
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
     let errors= form.querySelectorAll('.error')
 
     for (let i = 0; i < errors.length; i++) {
         errors[i].remove()
     }
- }
-
-
- let checkFields = function(){
+    
     for (let i = 0; i < fields.length; i++) {
-        if (!fields[i.value]){
-         console.log("fields is blank", fields[i])
-         let error = genErrors('cennot be blank')
-         form[i].parentElement.insertBefore(error, fields[i])
-        }
-     }
- }
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    removeErr()
-    checkFields ()
-    checkInput()
+       if (!fields[i.value]){
+        console.log("fields is blank", fields[i])
+        let error = document.createElement('div')
+        error.className="error"
+        from.style.border = "1px solid red"
+        error.innerHTML= "cennot be blank"
+        form[i].parentElement.insertBefore(error, fields[i])
+       }
+    }
+})
 
+
+from.addEventListener('input',function (arg) {
+    this.value = Number( this.value.substr(0,15))
+    if(isNaN(this.value)) {
+       
+    }
 })
