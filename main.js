1c0dfb12d4cdb4605ddad1cb87f9720507481170
AndriiGriMM <webdevgrimm@gmail.com>
2022-06-24 17:42:13 +0300

    24.06.22

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-let form = document.querySelector('.form')
-let submit = document.querySelector('.js--submit')
-let from = form.querySelector('.js--number')
-let fields = form.querySelectorAll('.input')
+$(document).ready(function () {
+    $('#next').click(function () {
 
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    let errors= form.querySelectorAll('.error')
-
-    for (let i = 0; i < errors.length; i++) {
-        errors[i].remove()
-    }
-    
-    for (let i = 0; i < fields.length; i++) {
-       if (!fields[i.value]){
-        console.log("fields is blank", fields[i])
-        let error = document.createElement('div')
-        error.className="error"
-        from.style.border = "1px solid red"
-        error.innerHTML= "cennot be blank"
-        form[i].parentElement.insertBefore(error, fields[i])
-       }
-    }
-})
-
-
-from.addEventListener('input',function (arg) {
-    this.value = Number( this.value.substr(0,15))
-    if(isNaN(this.value)) {
-       
-    }
+        let $activeImage = $('.active');
+        let $activeIndex = $('p.active').index()
+        let $nextStep = $activeIndex + 1
+        let $nextP = $('p').eq($nextStep)
+        if ($activeIndex !== $('p').length - 1) {
+            $activeImage.removeClass('active')
+            $nextP.addClass('active')
+        }
+    })
+    $('#prev').click(function () {
+        let $activeImage = $('.active');
+        let $activeIndex = $('p.active').index()
+        let $prevStep = $activeIndex - 1
+        let $prevP = $('p').eq($prevStep)
+        if ($activeIndex !== 0) {
+            $activeImage.removeClass('active')
+            $prevP.addClass('active')
+        }
+    })
 })
